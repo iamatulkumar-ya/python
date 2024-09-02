2473174e30eb8e67ddb56f70a69238fa56889cde
Atul Kumar Yadav <105991842+iamatulkumar-ya@users.noreply.github.com>
2024-09-02 23:14:40 +0530

    Update flask_app.py

--- PythonAnywhereFlaskAPI/flask_app.py
+++ PythonAnywhereFlaskAPI/flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request 
+from flask import Flask, request
 from http import HTTPStatus, client
 from src.endpoint_handler import generate_response, get_medicine_by_name, get_medicine_by_uses, get_medicine_by_composition
 import os
@@ -6,21 +6,21 @@ import markdown
 
 app = Flask('my_first_app')
 
-@app.route('/') 
+@app.route('/')
 def health_check():
     try:
-        apiStatus = "API Health is good. <br/><br/>" 
+        apiStatus = "API Health is good. <br/><br/>"
         endpointDeatils = ""
         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'README.md'), 'r') as f:
-            v = f.read()  
-            endpointDeatils = markdown.markdown(v) 
+            v = f.read()
+            endpointDeatils = markdown.markdown(v)
 
         return f"{apiStatus}{endpointDeatils}"
-    
+
     except Exception as e:
         return generate_response(HTTPStatus.INTERNAL_SERVER_ERROR, f"Exception occurred. Error - {str(e)}")
 
-
+@app.route('/v1/apitest/', methods=['GET'])
 @app.route('/v1/apitest', methods=['GET'])
 def api_test():
     try:
@@ -29,9 +29,9 @@ def api_test():
 
         if 'input_code' in reqJsonDataKeys and  isinstance(reqJsonDataKeys['input_code'], int):
             inputCode = reqJsonDataKeys['input_code']
-            try:  
+            try:
                 return  client.responses[inputCode],  HTTPStatus.__getitem__(str(client.responses[inputCode]).replace(" ", "_").upper())
-                  
+
             except:
                 return "Invalid Input", HTTPStatus.BAD_REQUEST
         else:
@@ -39,7 +39,8 @@ def api_test():
 
     except Exception as e:
         return generate_response(HTTPStatus.INTERNAL_SERVER_ERROR, f"Exception occurred. Error - {str(e)}")
- 
+
+@app.route('/v1/medicinebyname/', methods=['POST'])
 @app.route('/v1/medicinebyname', methods=['POST'])
 def medicine_by_name():
     try:
@@ -51,17 +52,17 @@ def medicine_by_name():
 
             if len(result) > 0:
                 return generate_response(HTTPStatus.OK, "OK", result)
-            
+
             else:
                 return generate_response(HTTPStatus.NOT_FOUND, "No record found")
-    
+
         else:
             return generate_response(HTTPStatus.BAD_REQUEST, "Bad Input. Input body is missing required 'medicine_name' key.")
 
     except Exception as e:
         return generate_response(HTTPStatus.INTERNAL_SERVER_ERROR, f"Exception occurred. Error - {str(e)}")
-    
 
+@app.route('/v1/medicinebyuses/', methods=['POST'])
 @app.route('/v1/medicinebyuses', methods=['POST'])
 def medicine_by_uses():
     try:
@@ -73,16 +74,17 @@ def medicine_by_uses():
 
             if len(result) > 0:
                 return generate_response(HTTPStatus.OK, "OK", result)
-            
+
             else:
                 return generate_response(HTTPStatus.NOT_FOUND, "No record found")
-    
+
         else:
             return generate_response(HTTPStatus.BAD_REQUEST, "Bad Input. Input body is missing required 'medicine_uses' key.")
 
     except Exception as e:
         return generate_response(HTTPStatus.INTERNAL_SERVER_ERROR, f"Exception occurred. Error - {str(e)}")
-    
+
+@app.route('/v1/medicinebycomposition/', methods=['POST'])
 @app.route('/v1/medicinebycomposition', methods=['POST'])
 def medicine_by_composition():
     try:
@@ -94,10 +96,10 @@ def medicine_by_composition():
 
             if len(result) > 0:
                 return generate_response(HTTPStatus.OK, "OK", result)
-            
+
             else:
                 return generate_response(HTTPStatus.NOT_FOUND, "No record found")
-    
+
         else:
             return generate_response(HTTPStatus.BAD_REQUEST, "Bad Input. Input body is missing required 'medicine_composition' key.")
 
